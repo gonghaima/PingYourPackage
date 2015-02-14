43a522f1bad9e2d714dec1244e04d5cf8a26bf90
gonghaima@hotmail.com <5418854188>
2015-02-15 10:15:44 +1300

    Initial commit - before   PingYourPackage.WebHost Project created

---
+++ PingYourPackage.API/Config/AutofacWebAPI.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+public class AutofacWebAPI
+{
+    public static void Initialize(HttpConfiguration config)
+    {
+        Initialize(config,
+        RegisterServices(new ContainerBuilder()));
+    }
+    public static void Initialize(
+    HttpConfiguration config, IContainer container)
+    {
+        config.DependencyResolver =
+        new AutofacWebApiDependencyResolver(container);
+    }
+    private static IContainer RegisterServices(ContainerBuilder builder)
+    {
+        builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+        // registration goes here
+        return builder.Build();
+    }
+}
